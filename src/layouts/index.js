1c205d32b990ee161d7da0b2aa62d4d6a35f535b
Paul Clifford <paul@Pauls-MacBook-Pro.local>
2018-04-14 00:16:47 -0400

    added home icon, transition effects

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,8 +1,9 @@
 import React from "react";
 import Link from "gatsby-link";
 import './index.scss'
+// import Logo from '../images/home.svg'; react component way?
 
-export default ({ children, data }) => 
+export default ({ children, data }) =>
   <div id="root">
     <Link className="site-title"to={`/`}>
       <h1>{data.site.siteMetadata.title}</h1>
