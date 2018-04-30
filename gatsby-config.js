8ee98db1025c67e62b0e2cc0c0596a37256ee1b9
Paul Clifford <clifford@queensu.ca>
2018-04-30 12:04:56 -0400

    adding netlify CMS

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,7 @@ module.exports = {
   plugins: [
     `gatsby-plugin-sass`,
     `gatsby-transformer-remark`,
+    `gatsby-plugin-netlify-cms`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
