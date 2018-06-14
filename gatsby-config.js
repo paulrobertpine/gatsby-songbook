8f284f8508bae2df60a16a89bbeb8df4bd1b29a7
paulrobertpine <paulrobertpine@gmail.com>
2018-06-14 14:34:45 -0400

    added manifest, offline support

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,11 +6,11 @@ module.exports = {
     `gatsby-plugin-sass`,
     `gatsby-transformer-remark`,
     `gatsby-plugin-netlify-cms`,
+    `gatsby-plugin-offline`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `src`,
-        // path: `${__dirname}/src/`,
         path: `${__dirname}/songs/`,
       },
     },
@@ -18,6 +18,18 @@ module.exports = {
       resolve: `gatsby-plugin-typography`,
       options: {
         pathToConfigModule: `src/utils/typography`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: "Jams!",
+        short_name: "Jams",
+        start_url: "/",
+        background_color: "#1c5a7e",
+        theme_color: "#c7c41d;",
+        display: "minimal-ui",
+        icon: "src/images/microphone.png",
       },
     },
   ],
