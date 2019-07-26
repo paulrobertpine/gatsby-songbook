ff1e27778c394071dbd8746a37a205dcd3eadd97
paulrobertpine <paulrobertpine@gmail.com>
2019-07-26 15:28:18 -0400

    update Netlify CMS

---
+++ src/pages/404.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Layout from '../components/Layout'
+
+class FourOFour extends React.Component {
+  render() {
+    return (
+      <Layout>
+        <div className="container">
+          <h1>404</h1>
+        </div>
+      </Layout>
+    )
+  }
+}
+
+export default FourOFour
