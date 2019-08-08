aba6b72a976d75fdfa0b7500527df3154958a238
paulrobertpine <paulrobertpine@gmail.com>
2019-08-08 10:48:35 -0400

    updates

--- songs/give-me-roses-while-i-live.md
+++ songs/give-me-roses-while-i-live.md
@@ -2,8 +2,11 @@
 title: Give Me Roses While I Live
 artist: The Carter Family
 key: G
-
+youtube: Y0kHjaTq8zo
 ---
+| [G]/ / / | / / / | / / / | / / / |
+| [D7]/ / / | / / / | [G]/ / / | / / / | (x2)
+
 [G]Wonderful things of folks are said
 [D7]When they have passed [G]away
 [G]Roses adorn the narrow bed
@@ -13,20 +16,20 @@ key: G
 [C]Give me the roses [G]while I live
 [G]Trying to cheer me [D7]on
 [G]Useless are flowers that you give
-[D7]After the soul is [G]gone
+[D7]After the soul has [G]gone
 
 (play chorus instrumental after each chorus)
-
-[G]Kind words are useless when folks lie
-[D7]Cold in a narrow [G]bed
-[G]Don't wait till death to speak kind words
-[D7]Now should the words be [G]said
-
-(chorus)
 
 [G]Let us not wait to do good deeds
 [D7]Till they have passed a-[G]way
 [G]Now is the time to sow good seeds
 [D7]While here on earth we [G]stay
+
+(chorus)
+
+[G]Kind words are useless when folks lie
+[D7]Cold in a narrow [G]bed
+[G]Don't wait till death to speak kind words
+[D7]Now should the words be [G]said
 
 (chorus x2)
--- src/pages/how-to.js
+++ src/pages/how-to.js
@@ -13,15 +13,54 @@ class HowTo extends React.Component {
 
         <div className="container">
           <h2>Creating and editing a song</h2>
-          <p>This website uses a system called  <a href="https://www.chordpro.org/chordpro/ChordPro-Chords.html">ChordPro</a> for adding chords to lyrics; it's a standard way to ensure that chords will show up in the right place and are able to be transposed, et.</p>
-          <h3>About ChordPro</h3>
-          <p>The ChordPro system works by adding a chord in sqaure brackets [] directly before the syllable where the chord is played.</p>
+          <p>
+            This website uses a system called{' '}
+            <a href="https://www.chordpro.org/chordpro/ChordPro-Chords.html">
+              ChordPro
+            </a>
+            for adding chords to lyrics; it's a standard way to ensure that
+            chords will show up in the right place and are able to be
+            transposed, et.
+          </p>
+          <p>
+            The ChordPro system works by adding a chord in sqaure brackets []
+            directly before the syllable where the chord is played:
+          </p>
+          <code>
+            Let it [Am]be, let it [C/G]be, let it [F]be, let it [C]be
+            <br />
+            [C]Whisper words of [G]wisdom, let it [F]be [C/E] [Dm] [C]
+          </code>
+          <p>
+            There is a helpful tool called{' '}
+            <a href="https://martijnversluis.github.io/ChordFiddle">
+              ChordFiddle
+            </a>{' '}
+            that will let you cut-and-paste in a standard lead sheet in and
+            convert to ChordPro format. It also lets you transpose the songs.
+          </p>
+
           <h2>Conventions</h2>
-          <h3>Inserting chords</h3>
           <h3>Showing rhythm</h3>
           <p>It can be helpful to show rhythm using barlines and beats like:</p>
-          <p>| / / / / | / / / / | / / / / | / / / / |</p>
+          <code>| / / / / | / / / / | / / / / | / / / / |</code>
           <p>is 4 bars in 4/4 time</p>
+          <p>You can use this to add chords to just like adding to lyrics:</p>
+          <code>| [C]/ / / / | [G]/ / / / | [F]/ / / / | [Am/ / / / |</code>
+          <p>useful for jammy bits without lyrics.</p>
+          <p>
+            You can also but the forward slashes right in the chord symbols when
+            you need to show rhythms over lyrics, like:
+          </p>
+          <code>
+            [F]Reflect the stars that [Am]guide me toward sal-[Em]va-[F / / / Am
+            /]tion
+          </code>
+          <p>
+            When a chord is in the middle of the word, use a hyphen right before
+            it:
+          </p>
+          <code>[G]I hear the drums [Bm]echoing to-[Em]night</code>
           <h2>GitHub workflow</h2>
         </div>
       </Layout>
