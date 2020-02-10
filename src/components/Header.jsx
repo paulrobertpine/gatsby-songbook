26762476c382c4581f6837872fbfdf7a58060721
paulrobertpine <paulrobertpine@gmail.com>
2020-02-10 16:12:50 -0500

    first shot at scroll speed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 import { Link } from 'gatsby'
 import SVG from 'react-inlinesvg'
 import logo from '../images/musician.svg'
-import { FiPlayCircle, FiPauseCircle } from 'react-icons/fi'
+import {
+  FiPlayCircle,
+  FiPauseCircle,
+  FiArrowRightCircle,
+  FiArrowLeftCircle,
+} from 'react-icons/fi'
 
 let scrolldelay = -1
 
@@ -13,12 +18,15 @@ class Header extends React.Component {
     this.startScrolling = this.startScrolling.bind(this)
     this.pauseScrolling = this.pauseScrolling.bind(this)
     this.setScroll = this.setScroll.bind(this)
-    this.state = { scrolling: true }
+    this.state = {
+      scrolling: true,
+      scrollSpeed: 50,
+    }
   }
 
   pageScroll() {
     window.scrollBy(0, 1) // horizontal and vertical scroll increments
-    scrolldelay = setTimeout(this.pageScroll, 120)
+    scrolldelay = setTimeout(this.pageScroll, this.state.scrollSpeed)
   }
 
   startScrolling(e) {
@@ -29,26 +37,61 @@ class Header extends React.Component {
     window.clearTimeout(scrolldelay)
     scrolldelay = -1
   }
-  
+
   setScroll(e) {
-    this.setState({ scrolling: !this.state.scrolling }) 
-    
-    if (this.state.scrolling) {  this.startScrolling(e) }
-    else { this.pauseScrolling(e) }
+    this.setState({ scrolling: !this.state.scrolling })
+
+    if (this.state.scrolling) {
+      this.startScrolling(e)
+    } else {
+      this.pauseScrolling(e)
+    }
+  }
+
+  increaseSpeed = e => {
+    if (this.state.scrollSpeed > 0) {
+      this.setState({
+        scrollSpeed: this.state.scrollSpeed - 10,
+      })
+    }
+  }
+
+  decreaseSpeed = e => {
+    this.setState({
+      scrollSpeed: this.state.scrollSpeed + 10,
+    })
   }
 
   render() {
-    
     return (
       <header id="site-header">
         <div className="container">
-          <Link to="/" className="site-title" onClick={this.pauseScrolling.bind(this)}>
+          <Link
+            to="/"
+            className="site-title"
+            onClick={this.pauseScrolling.bind(this)}
+          >
             <SVG src={logo} width="30" />
           </Link>
           <ul className="util-nav">
             <li>
+              <button onClick={this.decreaseSpeed}>
+                <FiArrowLeftCircle />
+              </button>
+            </li>
+            <li>
+              <span className="scroll-speed-label">
+                {this.state.scrollSpeed}
+              </span>
+            </li>
+            <li>
+              <button onClick={this.increaseSpeed}>
+                <FiArrowRightCircle />
+              </button>
+            </li>
+            <li>
               <button onClick={this.setScroll.bind(this)}>
-                { this.state.scrolling ? <FiPlayCircle /> : <FiPauseCircle /> }
+                {this.state.scrolling ? <FiPlayCircle /> : <FiPauseCircle />}
               </button>
             </li>
           </ul>
